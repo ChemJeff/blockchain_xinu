c2e19c4589c9d3d2cc6e7e1c00ae0d04f1b21c9a
ChemJeff <ChemJeff@pku.edu.cn>
2018-12-26 23:38:55 +0800

    initial framework

---
+++ blockchain.c
@@ -0,0 +1,24 @@
+#include <xinu.h>
+
+// 当前是否有对应过程正在处理中
+bool8 send_proc = FALSE;
+bool8 recv_proc = FALSE;
+bool8 contract_proc = FALSE;
+
+
+
+process udp_recvp() {
+    // foo
+}
+
+process recvp() {
+    // foo
+}
+
+process contractp() {
+    // foo
+}
+
+process main() {
+    // foo
+}
